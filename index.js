cf88321df9691784773d15eceb3085f1f7248e23
Andy <dis7@gmx.net>
2021-03-31 23:18:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,246 @@
+const canvasObj = document.querySelector("canvas");
+
+const ctx = canvasObj.getContext('2d');
+
+canvasObj.width = window.innerWidth;
+canvasObj.height = window.innerHeight;
+
+const scoreHtml = document.getElementById('scoreHtml');
+
+console.log(ctx);
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+class Bullet {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+const friction = 0.96; //low means gibs splatt! high means gibs will travel far!
+class Gib {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.alpha = 1;
+    }
+
+    draw() {
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.arc(
+            this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.restore();
+    }
+    update() {
+        this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.001
+        // Alpha can be really small. BUT: It needs to have at least SOME value...
+        // Or they will never be removed and the canvas will look like Jackson Pollock Art...
+        // And of course, the CPU will say Good-Bye sooner or later
+    }
+}
+
+const xWorld = canvasObj.width / 2;
+const yWorld = canvasObj.height / 2;
+
+const player = new Player(xWorld, yWorld, 30, "#070707");
+//console.log(player);
+const bullets = [];
+const enemies = [];
+const gibs = [];
+
+
+
+function spawnEnemies() {
+    setInterval(() => {
+        const x = Math.random() * canvasObj.width;
+        // Math.random() < 0.5 ? 0 : canvasObj.width;
+        const y = 0;
+        const radius = Math.random() * (30 - 5) + 5; // Max: 30, Min: 5
+
+        const color =  `hsl(${Math.random()*360}, 50%, 50%)`  // "#225588";
+
+        const angle = Math.atan2(yWorld - y, xWorld - x); //Closing IN to the Center/Player.
+        const velocity = { x: Math.cos(angle), y: Math.sin(angle)};
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 2000);
+}
+
+let score = 0;
+let animationId;
+function animate() {
+    animationId = requestAnimationFrame(animate);
+    ctx.fillStyle = "rgba(30, 30, 30, 0.2)"; //Color of Television, tuned to a dead channel.
+    ctx.fillRect(0, 0, canvasObj.width, canvasObj.height) // clear Screen
+    player.draw();  // Draw back the Player to the Screen
+
+    gibs.forEach((gib, gibIndex) => {
+        if (gib.alpha <= 0) {
+            gibs.splice(gibIndex, 1)    // Remove Gib when faded (alpha is 0)
+        } else {
+            gib.update();
+        }
+    });
+
+    bullets.forEach((bullet, bulletLocIndex) => {
+        bullet.update();
+
+        // Remove Bullets from Game when they are outside of the Screen:
+        if (bullet.x + bullet.radius < 0 ||
+            bullet.x - bullet.radius > canvasObj.width ||
+            bullet.y + bullet.radius < 0 ||
+            bullet.y - bullet.radius > canvasObj.height) {
+            setTimeout(() => {
+                    bullets.splice(bulletLocIndex, 1);
+                }, 0);
+        }
+    });
+
+    enemies.forEach((enemy, enemiesIndex) => {
+        enemy.update();
+
+        bullets.forEach((bullet, bulletIndex) => {
+            const distEnemBullet = Math.hypot(bullet.x - enemy.x, bullet.y - enemy.y);
+            // It's a Hit-Test, but mega accurate (checking 0 and 0)
+            // This is why the radius of both should check:
+
+            if (distEnemBullet - enemy.radius - bullet.radius < 1) {
+                    // Update Score:
+                    score += 100;
+                    scoreHtml.innerHTML = score;
+
+                    // Gib when Gibbed:
+                    for (let i = 0; i < enemy.radius * 3; i++) {
+                        gibs.push(new Gib(
+                            bullet.x,
+                            bullet.y,
+                            Math.random() * 2,
+                            enemy.color, // Change to "#992200" / "#AA1111" for "ultra-violent"
+                            {x: (Math.random() - 0.5) * (Math.random() * 4),
+                            y: (Math.random() - 0.5) * (Math.random() * 4)
+                            }))
+                    }
+                
+                if (enemy.radius - 5 > 10) {
+                    score += 10;
+                    // Make Big ones smaller when hit:
+                    // enemy.radius -= 5
+                    // Using the GSAP Library here, to make it much smoother:
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10   //Could be called "Attack Power"
+                    })
+                        setTimeout(() => {
+                        bullets.splice(bulletIndex, 1);
+                    }, 0);
+                } else {
+                        setTimeout(() => {
+                        console.log("We are touching!!");
+                        enemies.splice(enemiesIndex, 1);
+                        bullets.splice(bulletIndex, 1);
+                        //We give out indexes so the touching ones are removed.
+                    }, 0);
+                }
+
+            }
+        });
+
+        // This is Game Over:
+        const distEnemPlayer = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+            if (distEnemPlayer - enemy.radius - player.radius < 1) {
+                console.log("We are FCKD!");
+                cancelAnimationFrame(animationId);
+            }
+    });
+}
+
+//All mouse properties are visible,
+//when we add event to parameter and log it.
+//Things like clientX and clientY(the mouse position!)
+window.addEventListener("click", (event) => {
+    console.log(bullets);
+    const angle = Math.atan2(event.clientY - yWorld, event.clientX - xWorld);
+    // console.log(angle);
+
+    // Bullet Speed:
+    let speed = 5;
+    const velocity = {
+        x: Math.cos(angle) * speed,
+        y: Math.sin(angle) * speed
+    };
+
+    bullets.push(new Bullet(
+        xWorld, yWorld, 3, "#DDBB99", velocity
+        ));
+
+    console.log(event);
+});
+
+animate();
+spawnEnemies();
